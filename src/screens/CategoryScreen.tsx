f4941040e2d1812f6cfd934f1e9bf4d21aaddf08
Bahram <baxram1997007@gmail.com>
2024-03-03 03:13:15 +0400

    update...

--- src/screens/CategoryScreen.tsx
+++ src/screens/CategoryScreen.tsx
@@ -64,85 +64,85 @@ const CategoryScreen = ({ navigation }: CategoryScreenProps) => {
   };
 
   return (
-    // <View style={styles.container}>
-    //   <View style={styles.categoryContainer}></View>
-    //   <TextInput
-    //     style={styles.searchInput}
-    //     value={searchTerm}
-    //     onChangeText={setSearchTerm}
-    //     placeholder="Search..."
-    //   />
-    //   <ScrollView contentContainerStyle={styles.itemCardContainer}>
-    //     {filteredData.map((item, index) => (
-    //       <TouchableOpacity
-    //         style={styles.itemCard}
-    //         key={index}
-    //         onPress={() => handlePress(item.title)}
-    //       >
-    //         <Image source={item.image} style={styles.image} />
-    //         <Text style={styles.title}>{item.title}</Text>
-    //       </TouchableOpacity>
-    //     ))}
-    //   </ScrollView>
-    // </View>
+    <View style={styles.container}>
+      <View style={styles.categoryContainer}></View>
+      <TextInput
+        style={styles.searchInput}
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+        placeholder="Search..."
+      />
+      <ScrollView contentContainerStyle={styles.itemCardContainer}>
+        {filteredData.map((item, index) => (
+          <TouchableOpacity
+            style={styles.itemCard}
+            key={index}
+            onPress={() => handlePress(item.title)}
+          >
+            <Image source={item.image} style={styles.image} />
+            <Text style={styles.title}>{item.title}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
 export default CategoryScreen;
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   categoryContainer: {
-//     width: windowWidth * 0.9,
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//     justifyContent: "space-between",
-//     alignItems: "center",
-//     rowGap: 20,
-//   },
-//   category: {
-//     fontSize: 20,
-//     fontWeight: "bold",
-//   },
-//   searchInput: {
-//     position: "relative",
-//     width: 370,
-//     height: 45,
-//     left: 0,
-//     top: 0,
-//     borderRadius: 10,
-//     backgroundColor: "#F6F5F5",
-//     paddingLeft: 15,
-//     fontSize: 18,
-//   },
-//   itemCardContainer: {
-//     // flex: 1,
-//     flexDirection: "row",
-//     flexWrap: "wrap",
-//     justifyContent: "space-between",
-//   },
-//   itemCard: {
-//     width: "45%",
-//     marginBottom: 20,
-//     borderRadius: 10,
-//     overflow: "hidden",
-//     borderWidth: 1,
-//     borderColor: "#ccc",
-//   },
-//   image: {
-//     width: "100%",
-//     height: 200,
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: "bold",
-//     marginTop: 10,
-//     paddingHorizontal: 10,
-//   },
-//   count: {
-//     paddingHorizontal: 10,
-//     marginBottom: 10,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  categoryContainer: {
+    width: windowWidth * 0.9,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
+    rowGap: 20,
+  },
+  category: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  searchInput: {
+    position: "relative",
+    width: 370,
+    height: 45,
+    left: 0,
+    top: 0,
+    borderRadius: 10,
+    backgroundColor: "#F6F5F5",
+    paddingLeft: 15,
+    fontSize: 18,
+  },
+  itemCardContainer: {
+    // flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  itemCard: {
+    width: "45%",
+    marginBottom: 20,
+    borderRadius: 10,
+    overflow: "hidden",
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    paddingHorizontal: 10,
+  },
+  count: {
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+});
